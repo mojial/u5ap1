aabef9064368127148509a76c8d42e9bb82d1cc4
mojial <mojial@floridauniversitaria.es>
2023-01-23 11:27:10 +0100

    ej 1 grupo 1

--- uno.sh
+++ uno.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "hola, dime tu fecha de nacimineto"
+echo "Escribe tu nombre"
+read nombre
+
+echo "Escribe tu primer apellido"
+read apellido1
+
+echo "Escribe tu segundo apellido"
+read apellido2
+
+sleep 5
+
+echo "Tu nombre completo es $name $apellido1 $apellido2"
